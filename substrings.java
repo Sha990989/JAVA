ec23d358d8939f805fad46c22f561b113b964391
A.Navaneetha <anavaneethasankar@gmail.com>
2025-02-14 10:46:52 +0530

    Add files via upload

---
+++ substrings.java
@@ -0,0 +1,9 @@
+public class substrings {
+    public static void main(String args[])
+    {
+        String s="hello";
+        String s2=s.substring(1,5);
+        System.out.println(s2);
+    }
+    
+}
