fa996903683614b2f6b377c69cb03c4573e75a0f
A.Navaneetha <anavaneethasankar@gmail.com>
2025-02-14 10:44:49 +0530

    Add files via upload

---
+++ staticblock.java
@@ -0,0 +1,10 @@
+public class staticblock {
+    static{
+        System.out.println("this is static block");
+    }
+    public static void main(String args[])
+    {
+        System.out.println("this is main block");
+    }
+    
+}
