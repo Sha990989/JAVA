f4ef36ac254ab1c5efdbd5dd22d7450f7180b099
A.Navaneetha <anavaneethasankar@gmail.com>
2025-02-14 10:39:37 +0530

    Add files via upload

---
+++ person.java
@@ -0,0 +1,15 @@
+public class person {
+ person()
+ {
+    System.out.print("Base Class");
+ }   
+}
+class student extends person
+{
+    student()
+    {
+        super();
+        System.out.println("sub class");
+    }
+}
+class 
