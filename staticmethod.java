ce4f9077ccee4df8606cabd0c77b98f5ad51c094
A.Navaneetha <anavaneethasankar@gmail.com>
2025-02-14 10:45:40 +0530

    Add files via upload

---
+++ staticmethod.java
@@ -0,0 +1,13 @@
+public class staticmethod {
+    public int x=50;
+     void display()
+        {
+            System.out.println("x="+x);
+        }
+        public static void main(String args[])
+        {
+            staticmethod a=new staticmethod();
+            a.display();
+        }
+    
+}
