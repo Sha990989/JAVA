4b2117b603835a800c86b30ede16bc978c553d6d
A.Navaneetha <anavaneethasankar@gmail.com>
2025-02-14 10:44:34 +0530

    Add files via upload

---
+++ static.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class staticblock 
+{
+static
+{
+System.out.print("THIS IS STATIC BLOCK");
+}
+public static void mian(String[] args)
+{
+System.out.print("THIS IS MAIN BLOCK");
+}
+}
