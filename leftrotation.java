e65acaee0b46d05675af7ae4dc6a919d7736acb5
A.Navaneetha <anavaneethasankar@gmail.com>
2025-02-14 10:37:29 +0530

    Add files via upload

---
+++ leftrotation.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class leftrotation 
+{
+    public static void main(String args[])
+    {
+        int nums[]={1,2,3,4,5,6,7};
+        int n=nums.length;
+        int k=2;
+        int temp[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            temp[(i-k+n)%n]=nums[i];    
+        }
+        System.arraycopy(temp,0,nums,0,n);
+        for(int i=0;i<nums.length;i++)
+        {
+            System.out.print(nums[i]+" ");
+        }
+    }
+    
+}
