b0ecf720c94ed0d9c83058d397376e7c1c60a524
A.Navaneetha <anavaneethasankar@gmail.com>
2025-02-14 10:46:31 +0530

    Add files via upload

---
+++ strings.java
@@ -0,0 +1,9 @@
+public class strings {
+    public static void main(String args[])
+
+    {
+        StringBuffer b1=new StringBuffer("hello ");
+        b1.append("world");
+        System.out.println(b1);
+    }
+}
