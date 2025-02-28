4099f59a23ec3a1e1cd32b1de3e714e68acaf64e
A.Navaneetha <anavaneethasankar@gmail.com>
2025-02-28 10:31:24 +0530

    Add files via upload

---
+++ printASCIIvalue.java
@@ -0,0 +1,10 @@
+public class printASCIIvalue {
+    public static void main(String args[])
+    {
+        char ch='A';
+        int ascii=(int)ch;
+        System.out.println(ascii);
+        
+    }
+    
+}
